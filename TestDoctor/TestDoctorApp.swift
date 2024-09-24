61f8b38a68c7014f513a407f342fb9bd9b2144bd
Юрий <145429474+yuriIosDev@users.noreply.github.com>
2024-09-24 10:44:06 +0300

    Initial Commit

---
+++ TestDoctor/TestDoctorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDoctorApp.swift
+//  TestDoctor
+//
+//  Created by Юрий Кузнецов on 24.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestDoctorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
